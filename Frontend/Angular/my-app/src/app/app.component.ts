959a147259c6152048c6b0b74452f57bfd1a1087
Anamika Lodhi <anamikalodhi77@gmail.com>
2026-07-14 16:52:22 +0530

    Angular practice

---
+++ Frontend/Angular/my-app/src/app/app.component.ts
@@ -0,0 +1,72 @@
+import { Component } from '@angular/core';
+
+import { LoginComponent } from './login/login.component';
+import { SignupComponent } from './signup/signup.component';
+import { ProfileComponent } from './profile/profile.component';
+import { EventsComponent } from './events/events.component';
+
+
+@Component({
+  selector: 'app-root',
+  imports: [LoginComponent, SignupComponent, ProfileComponent, EventsComponent],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.css'
+})
+export class AppComponent {
+  // title = 'my-app'; knwon as properties do not define using const or let class ke adr prop bhr variable 
+  // can use variable inside class using func
+  // title = 'Anamika Lodhi';
+  // name = "Annu";
+  // x = 10;
+  // y = 10;
+  // user1 = 'Annu';
+  // user2 = 'Anamika';
+
+  onClick(){
+    console.log("Function called");
+    this.otherFunction();
+  }
+
+  otherFunction(){
+    console.log("Other function called");
+  }
+
+  // property, if not write string here no problem ts understand it
+  name:string="Anamika Lodhi";
+  data:string|number="Hello";
+  other:any= true;       // when not knwo the datatype then take any
+
+  updateName(){
+    let x= 30;            // variable 
+    let y:number= 30;            // variable
+    this.name = "Annu";
+    this.data = 100;
+    this.other = 10;
+  }
+
+  sum(a: number, b: number){
+    console.log(a+b);
+  }
+
+  count = 0;
+  // handleIncrement(){
+  //   this.count++;
+  // }
+
+  // handleDecrement(){
+  //   this.count--;
+  // }
+
+  // handleReset(){
+  //   this.count=0;
+  // }
+
+  handleCounter(val:string){
+    if(val== 'minus'){
+      this.count--;
+    }else if(val== 'plus'){
+      this.count++;
+    }else
+      this.count =0;
+  }
+}
---
+++ Frontend/Angular/my-app/src/app/events/events.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-events',
+  imports: [],
+  templateUrl: './events.component.html',
+  styleUrl: './events.component.css'
+})
+export class EventsComponent {
+
+  handleEvent(event: MouseEvent){
+    console.log("Event handled", event.type);
+    console.log("Event target", (event.target as Element).className);
+  }
+}
