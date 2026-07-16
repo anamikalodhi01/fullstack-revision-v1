f2463595c3b2c9d04a38cd08cf55b91b82563f5c
Anamika Lodhi <anamikalodhi77@gmail.com>
2026-07-16 15:11:39 +0530

    create application layout

---
+++ Frontend/Angular/project/moneymate/src/app/app.component.ts
@@ -0,0 +1,17 @@
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+
+import  { HeaderComponent } from './shared/components/header/header.component';
+import  { SidebarComponent } from './shared/components/sidebar/sidebar.component';
+import  { FooterComponent } from './shared/components/footer/footer.component';
+
+@Component({
+  selector: 'app-root',
+  standalone: true,
+  imports: [RouterOutlet, HeaderComponent, SidebarComponent, FooterComponent],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.css'
+})
+export class AppComponent {
+  title = 'moneymate';
+}
